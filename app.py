8cbed15a60f69383ae70ef0021e0c2386d92d493
김수빈 <bz.subin@gmail.com>
2026-03-22 17:44:10 +0900

    주소 수정

--- app.py
+++ app.py
@@ -306,4 +306,4 @@ async def get_data(db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5909)
+    uvicorn.run(app, host="127.0.0.1", port=5909)
